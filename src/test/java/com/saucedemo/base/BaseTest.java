2a26b26b8a29f663b4d6edcd3cbbf784791f1e48
sathvik200 <48060934+sathvik200@users.noreply.github.com>
2025-09-16 12:08:07 +0530

    Update BaseTest.java

--- src/test/java/com/saucedemo/base/BaseTest.java
+++ src/test/java/com/saucedemo/base/BaseTest.java
@@ -1,9 +1,7 @@
 package com.saucedemo.base;
 
 import com.saucedemo.utils.ConfigReader;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
@@ -19,40 +17,43 @@ public class BaseTest {
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
     public ConfigReader configReader;
 
-    public static WebDriver getDriver(){
+    public static WebDriver getDriver() {
         return driver.get();
     }
 
     @BeforeMethod
-    public void setup(){
+    public void setup() {
         configReader = new ConfigReader();
-        Map<String, Object> prefs = new HashMap<String, Object>();
+
+        // Disable password saving popups
+        Map<String, Object> prefs = new HashMap<>();
         prefs.put("credentials_enable_service", false);
         prefs.put("profile.password_manager_enabled", false);
-
         prefs.put("profile.password_manager_leak_detection_enabled", true);
 
         ChromeOptions options = new ChromeOptions();
         options.setExperimentalOption("prefs", prefs);
         options.addArguments("--disable-save-password-bubble");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
 
-        String userDataDir = System.getProperty("user.dir") + "/target/chrome-profile";
-        options.addArguments("--user-data-dir=" + userDataDir);
-
-        WebDriverManager.chromedriver().setup();
         try {
-            // Use RemoteWebDriver, pointing to the Grid URL and passing the options
-            driver.set(new RemoteWebDriver(new URL("http://localhost:4444"), options));
+            // Connect to Selenium Grid Hub
+            driver.set(new RemoteWebDriver(
+                    new URL("http://localhost:4444/wd/hub"), // ✅ correct endpoint
+                    options
+            ));
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
+
         getDriver().manage().window().maximize();
         getDriver().get(configReader.getProperty("baseUrl"));
     }
 
     @AfterMethod
-    public void TearDown(){
-        if(getDriver()!=null){
+    public void tearDown() {
+        if (getDriver() != null) {
             getDriver().quit();
             driver.remove();
         }
